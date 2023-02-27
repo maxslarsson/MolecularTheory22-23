335224b4fbe4ec1bf51940feaae8108f06e30380
Max Larsson <maxslarsson@gmail.com>
2023-02-26 19:59:14 -0500

    Add all the constants for the intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeandlift/IntakeConstants.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.intakeandlift;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class IntakeConstants {
+    public static double[] STACK_HEIGHTS = {
+            3, // 1st cone
+            150, // 2nd cone
+            202, // 3rd cone
+            305, // 4th cone
+            403, // 5th cone
+    };
+
+    public static double HIGH_JUNCTION_HEIGHT = 2200;
+    public static double MEDIUM_JUNCTION_HEIGHT = 1600;
+    public static double LOW_JUNCTION_HEIGHT = 1000;
+    public static double GROUND_JUNCTION_HEIGHT = 140;
+    public static double BOTTOM = 0;
+
+    public static double ON_JUNCTION_HEIGHT_CHANGE = 300;
+
+    public static double INTAKE_CLAW_OPEN_POSITION = 0.5;
+    public static double INTAKE_CLAW_CLOSED_POSITION = 0.35;
+    public static double INTAKE_ARM_DOWN_POSITION = 0.25;
+    public static double INTAKE_ARM_UP_POSITION = 0.75;
+
+    public static double LIFT_CLAW_OPEN_POSITION = 0.5;
+    public static double LIFT_CLAW_CLOSED_POSITION = 0.35;
+    public static double LIFT_CLAW_PLACING_CONE_ROTATION = 0.25;
+    public static double LIFT_CLAW_PICKING_UP_CONE_POSITION = 0.75;
+    public static double LIFT_HEIGHT_TO_ROTATE_CLAW = 400;
+}
