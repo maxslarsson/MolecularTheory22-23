703d35ec7ea94e78d633272396fab06b56dc515a
Max Larsson <maxslarsson@gmail.com>
2023-02-26 19:59:05 -0500

    ADd code for intake mechanism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeandlift/Intake.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.intakeandlift;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class Intake {
+    public Servo clawServo;
+    public Servo leftServo;
+    public Servo rightServo;
+
+    public Intake(HardwareMap hardwareMap) {
+        clawServo = hardwareMap.get(Servo.class, "intakeClawServo");
+        leftServo = hardwareMap.get(Servo.class, "intakeLeftServo");
+        rightServo = hardwareMap.get(Servo.class, "intakeRightServo");
+
+        rightServo.setDirection(Servo.Direction.REVERSE);
+    }
+
+    public void setClawPosition(double position) {
+        clawServo.setPosition(position);
+    }
+
+    public void setArmPosition(double position) {
+        leftServo.setPosition(position);
+        rightServo.setPosition(position);
+    }
+}
