af97837a1eca4552d0a5d2fb0d9841b397713ff8
Max Larsson <maxslarsson@gmail.com>
2023-03-03 23:21:51 -0500

    Add ParkingAuto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,11 +1,14 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+    public static Pose2d START_POSE = new Pose2d(36, -62.8, Math.toRadians(270));
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -13,15 +16,9 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(START_POSE)
+                                .lineToSplineHeading(new Pose2d(54, -60, Math.toRadians(270)))
+                                .splineToConstantHeading(new Vector2d(60, -24), Math.toRadians(90))
                                 .build()
                 );
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ParkingAuto.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.PoseStorage;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.vision.AprilTagPipeline;
+import org.firstinspires.ftc.teamcode.vision.CameraController;
+
+@Config
+@Autonomous(preselectTeleOp = "Drive TeleOp")
+public class ParkingAuto extends LinearOpMode {
+    public static Pose2d START_POSE = new Pose2d(36, -62.8, Math.toRadians(270));
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        CameraController cameraController = new CameraController(hardwareMap);
+        AprilTagPipeline aprilTagPipeline = new AprilTagPipeline();
+        cameraController.setPipeline(aprilTagPipeline);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(START_POSE);
+
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("Parking Position", aprilTagPipeline.getParkingPosition());
+            telemetry.update();
+            sleep(25);
+        }
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        TrajectorySequence driveToParkingPosition;
+
+        switch (aprilTagPipeline.getParkingPosition()) {
+            case ZONE1:
+                driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
+                        .lineToSplineHeading(new Pose2d(18, -60, Math.toRadians(270)))
+                        .splineToConstantHeading(new Vector2d(12, -24), Math.toRadians(90))
+                        .build();
+                break;
+            default: // ZONE2 and NO_TAGS_SEEN
+                driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
+                        .setReversed(true)
+                        .splineTo(new Vector2d(36, -24), Math.toRadians(90))
+                        .build();
+                break;
+            case ZONE3:
+                driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
+                        .lineToSplineHeading(new Pose2d(54, -60, Math.toRadians(270)))
+                        .splineToConstantHeading(new Vector2d(60, -24), Math.toRadians(90))
+                        .build();
+                break;
+        }
+
+        drive.followTrajectorySequence(driveToParkingPosition);
+
+        PoseStorage.currentPose = drive.getPoseEstimate();
+    }
+}
